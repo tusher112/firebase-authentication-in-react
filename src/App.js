9c91f6c1139a7f3e14cdb1f19fbaa2f8baeada4c
tusher112 <mdtusherahmed820@gmail.com>
2022-04-10 02:30:40 +0600

    simple form validation

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
 
   const [email, setEmail]=useState('');
   const[pass, setPass]=useState('');
+  const [validated, setValidated] = useState(false);
+  const [error,setError]=useState('');
+
 
   const handleEmail= event =>{
     setEmail(event.target.value)
@@ -27,6 +30,23 @@ function App() {
   }
 
   const handleSubmit=event=>{
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      
+      event.stopPropagation();
+      return;
+    }
+      if(!/^(?=.*[!@#$%^&*()--+={} ])/.test(pass)){
+        setError('Password should contain al least one special character.')
+        return;
+      }
+
+
+
+    setValidated(true);
+    setError('');
+
     createUserWithEmailAndPassword(auth, email, pass)
     .then(result=>{
       const user=result.user;
@@ -41,22 +61,30 @@ function App() {
     <div>
       <div className="registration w-50 mx-auto mt-5">
         <h2 className='text-primary'>Please register!</h2>
-      <Form onSubmit={handleSubmit}>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" />
+    <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" required/>
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text>
+    <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control onBlur={handlePass} type="password" placeholder="Password" />
+    <Form.Control onBlur={handlePass} type="password" placeholder="Password" required />
+  
+    <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
   </Form.Group>
   {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
     <Form.Check type="checkbox" label="Check me out" />
   </Form.Group> */}
+  <p className='text-danger'>{error}</p>
   <Button variant="primary" type="submit">
     Submit
   </Button>
